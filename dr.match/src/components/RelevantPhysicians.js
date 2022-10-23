f215213f6df97229d0313d4984e67213695f339b
isaac-w18 <isaacw1218@gmail.com>
2022-10-23 09:52:42 -0400

    modified:   dr.match/src/components/DrBethAppiah.png 	modified:   dr.match/src/components/DrBrianEdwards.png 	modified:   dr.match/src/components/DrVishwanathHande.png 	new file:   dr.match/src/components/RelevantPhysicians.css 	modified:   dr.match/src/components/RelevantPhysicians.js

--- dr.match/src/components/RelevantPhysicians.js
+++ dr.match/src/components/RelevantPhysicians.js
@@ -1,24 +1,29 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import "./RelevantPhysicians.css";
+
+
 import DrVishwanathHande from './DrVishwanathHande.png';
 import DrBethAppiah from './DrBethAppiah.png';
 import DrBrianEdwards from './DrBrianEdwards.png';
-console.log(DrVishwanathHande);
 
 const relevant = () => {
     return (
         <div>
             <header className="Relevant-header">
             </header>
-            <p>Here are relevant physicians we found for you!</p>
-            <img src={DrVishwanathHande} alt="DrVishwanathHande"></img>
-            <img src={DrBethAppiah} alt="DrBethAppiah"></img>
-            <img src={DrBrianEdwards} alt="DrBrianEdwards"></img>
+            <p id="relTopMes">Here are relevant physicians we found for you!</p>
+            <img id="vish" src={DrVishwanathHande} alt="DrVishwanathHande"></img>
             <h4 id="vishwanath">Dr. Vishwanath Hande</h4>
-            <h4 id="beth">Dr. Beth Appiah</h4>
-            <h4 id="brian">Dr. Brian Edwards</h4>
             <p id="vishwanath">InStride Foot & Ankle</p>
-            <p id="beth">InStride Foot & Ankle</p>
-            <p id="brian">Physical Therapist</p>
+
+            <img id="beth" src={DrBethAppiah} alt="DrBethAppiah"></img>
+            <h4 id="bethA">Dr. Beth Appiah</h4>
+            <p id="bethA">InStride Foot & Ankle</p>
+
+            <img id="brian" src={DrBrianEdwards} alt="DrBrianEdwards"></img>
+            <h4 id="brianE">Dr. Brian Edwards</h4>
+            <p id="brianE">Physical Therapist</p>
         </div>
     );
 };
