cbb817837f2e9beb339396496286073f23276159
isaac-w18 <isaacw1218@gmail.com>
2022-10-22 21:03:45 -0400

    Plantar Creation

--- dr.match/src/App.js
+++ dr.match/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './components/Home.js';
 import Symptoms from './components/Symptoms.js';
+import PlantarFasciitis from './components/PlantarFasciitis.js';
 import {
   BrowserRouter as Router,
   Routes,
@@ -21,11 +22,15 @@ function App() {
           with exact path "/about", in component 
           props we passes the imported component*/}
           <Route path="/symptoms" element={<Symptoms />} />
+
+          {/* This route is for about component 
+          with exact path "/about", in component 
+          props we passes the imported component*/} 
+          <Route path="/plantar" element={<PlantarFasciitis />} />
         
           {/* If any route mismatchesx the upper 
           route endpoints then, redirect triggers 
           and redirects app to home component with to="/" */}
-
         </Routes>
       </Router>
     </>
---
+++ dr.match/src/components/PlantarFasciitis.js
@@ -0,0 +1,24 @@
+import React from "react";
+const plantar = () => {
+      return (
+        <div>
+            <header className="Plantar-header">
+            </header>
+          <h1>Plantar Fasciitis</h1>
+          <p>Symptoms:</p>
+            <ul>
+                <li>Pain in the arch and/or heel of the foot</li>
+                <li>Tenderness and swelling</li>
+                <li>Inflammation</li>
+            </ul>
+            <p>Causes:</p>
+            <ul>
+                <li>Pain in the arch and/or heel of the foot</li>
+                <li>Tenderness and swelling</li>
+                <li>Inflammation</li>
+            </ul>
+        </div>
+      );
+    };
+
+export default plantar;
