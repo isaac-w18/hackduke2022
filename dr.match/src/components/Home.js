6c7b5c89c2fff1caa013c8849608f1c2a7d6855d
kristincounterman <107945409+kristincounterman@users.noreply.github.com>
2022-10-23 11:15:13 -0400

    buttons formatted

--- dr.match/src/components/Home.js
+++ dr.match/src/components/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 import drMatch from './drMatch.png';
+import './logo.css'
 
 console.log(drMatch);
 
@@ -8,12 +11,24 @@ console.log(drMatch);
 // importing Link from react-router-dom to navigate to 
 // different end points.
 const Home = () => {
+  const navigate = useNavigate();
+  const onSubmit = event => {
+      navigate('/plantar')
+  }
   return (
-    <div>
+    <div >
       <div>
         <header className="App-header">
-        <img src={drMatch} alt="drMatch"></img>
-        <h1>Dr. Match</h1> 
+        <img src={drMatch} alt="drMatch" className = "photo"></img>
+        {/* <h1>Dr. Match</h1>  */}
+        <form onSubmit={onSubmit}>
+
+        <label>
+          Tell us about your symptoms:
+        <input type="text" className = "input"/>
+        </label>
+        <button type='submit' >Submit</button>
+        </form>
         </header>
       </div>
       <div>
--- dr.match/src/components/InsuranceDropDown.js
+++ dr.match/src/components/InsuranceDropDown.js
@@ -1,12 +1,28 @@
-import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import {React, useState} from "react";
+import { Outlet, Link, Form, useNavigate } from "react-router-dom";
 
+const insurance = ["Cigna", "BlueCross/BlueShield", "Aetna"];
 const InsuranceDropDown = () => {
+  const [selected,setSelected] = useState();
+  const navigate = useNavigate();
+  const onSubmit = event => {
+    navigate('/relevant')
+}
       return (
         <div>
-            <header className="Insurance-drop-down">
-            </header>
-          <h1>Insurance drop down bar here</h1>
+            {/* <header className="Insurance-drop-down">
+            </header> */}
+          <h1>Select your insurance to find a provider:</h1>
+          <form onClick = {onSubmit}>
+            <select value = {selected} onChange = {(e) => setSelected(e.target.value)}>
+              {insurance.map((value) => 
+              (<option vaue={value} key={value}>
+                {value}
+              </option>)
+              )}
+              
+            </select>
+          </form>
         </div>
       );
     };
