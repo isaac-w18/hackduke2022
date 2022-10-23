ab8a5116eeb3fed502c8e76bc688de6967e296a7
Richard Cui <richardcui@Richards-MacBook-Pro.local>
2022-10-23 10:30:59 -0400

    richard changes

--- dr.match/src/components/Naproxen.js
+++ dr.match/src/components/Naproxen.js
@@ -6,8 +6,8 @@ const naproxen = () => {
             <header className="Naproxen-header">
             </header>
           <h1>Naproxen</h1>
-          <p>What is Naproxen?</p>
-            <ul>
+          <p id = "whatis">What is Naproxen?</p>
+            <ul id = "wnaproxen">
                 <li>Brand name: Aleve</li>
                 <li>In the class of Non-Steroidal Anti-Inflammatory Drugs (NSAIDs)</li>
                 <li>Relieves arthirtis, swelling, stiffness, and joint pain</li>
@@ -15,8 +15,8 @@ const naproxen = () => {
                 <li>Available in tablets and capsules</li>
                 <li>Generally very safe</li>
             </ul>
-            <p>How should I take Naproxen?</p>
-            <ul>
+            <p  id = "whatis">How should I take Naproxen?</p>
+            <ul id = "wnaproxen">
                 <li>Joint conditions: 500mg to 1,000mg a day</li>
                 <li>Muscle and bone disorders and pains: 500mg at first, then 250mg every 6 to 8 hours as needed.</li>
                 <li>Attacks of gout: 750mg, then 250mg every 8 hours until the attack has passed.</li>
--- dr.match/src/components/ibuprofen.js
+++ dr.match/src/components/ibuprofen.js
@@ -7,16 +7,16 @@ const ibuprofen = () => {
             <header className="Ibuprofen-header">
             </header>
           <h1>Ibuprofen</h1>
-          <p>What is Ibuprofen?</p>
-            <ul>
+          <p id="whatis">What is Ibuprofen?</p>
+            <ul id = "wibuprofen">
                 <li>Brand name: Advil, Motrin</li>
                 <li>In the class of Non-Steroidal Anti-Inflammatory Drugs (NSAIDs)</li>
                 <li>Used to relieve arthirtis, swelling, and minor pain</li>
                 <li>Available over-the-counter and as prescription</li>
                 <li>Generally very safe</li>
             </ul>
-            <p>How should I take Ibuprofen?</p>
-            <ul>
+            <p id="whatis">How should I take Ibuprofen?</p>
+            <ul id = "wibuprofen">
                 <li>Oral tablet: 200mg every 4-6 hours, unless otherwise directed by a doctor</li>
                 <li>Take with food if possible</li>
                 <li>Risks: Occasionally causes stomach bleeding. Do not take if you’ve ever had heart disease/heart attack</li>
